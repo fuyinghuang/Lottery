9850cc3694f5382712ca646d2e3c301767897362
huangfuin11 <huangfuin1101@yahoo.com.tw>
2020-05-26 23:31:33 +0800

    created Lottery component

--- lottery/src/App.js
+++ lottery/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
-import Ball from "./Ball";
+import "./App.css";
+import Lottery from "./Lottery";
 
-class Lottery extends Component {
+class App extends Component {
   render() {
     return (
-      <section className='Lottery'>
-            <Ball num={17} />
-      </section>
+      <div className='App'>
+        <Lottery />
+        <Lottery title='Mini Daily' maxNum={10} numBalls={4} />
+      </div>
     );
   }
 }
 
-export default Lottery;
+export default App;
